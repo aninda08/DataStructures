a29969ce14ebe62dd9fe22bff07dd6e2b38a826a
Aninda Chakraborty <aninda.death@gmail.com>
2021-02-02 02:16:08 +0530

    Initial commit

--- src/main/java/com/aninda/datastructure/DemoHandler.java
+++ src/main/java/com/aninda/datastructure/DemoHandler.java
@@ -1,5 +1,7 @@
 package com.aninda.datastructure;
 
 public class DemoHandler {
-    System.out.println("Welcome to DataStructures on java @AnindaSundarChakraborty");
+    public static void main(String args[]) {
+        System.out.println("Welcome to DataStructures on java @AnindaSundarChakraborty");
+    }
 }
